c5f450e188aa4284dd3f207fa081058fc77b35ea
Lucas Rufino <lucaslrt@outlook.com>
2017-03-24 15:01:16 -0300

    Signed-off-by: pwener <phwener@gmail.com> Signed-off-by: André Bedran <affbedran@gmail.com> Signed-off-by: Lucas Rufino <lucaslrt@outlook.com>
    Continuing authentication.

--- grails-app/controllers/investill/UserController.groovy
+++ grails-app/controllers/investill/UserController.groovy
@@ -3,7 +3,10 @@ package investill
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+import org.springframework.security.access.annotation.Secured
+
 @Transactional(readOnly = true)
+@Secured(['ROLE_ADMIN'])
 class UserController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
