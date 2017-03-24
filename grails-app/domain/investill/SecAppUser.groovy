7c98e7139594684ab7a8358e8e77e99d66f61627
pwener <phwener@gmail.com>
2017-03-24 11:43:26 -0300

    Installing spring.sec

---
+++ grails-app/domain/investill/SecAppUser.groovy
@@ -0,0 +1,49 @@
+package investill
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=false)
+class SecAppUser implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	transient springSecurityService
+
+	String username
+	String password
+	boolean enabled = true
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	Set<SecAppRole> getAuthorities() {
+		SecAppUserSecAppRole.findAllBySecAppUser(this)*.secAppRole
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+	}
+
+	static transients = ['springSecurityService']
+
+	static constraints = {
+		password blank: false, password: true
+		username blank: false, unique: true
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+}
